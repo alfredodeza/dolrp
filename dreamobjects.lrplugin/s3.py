75bea6d9aa11eaf8ffab3b87fb2ccd64aafca43f
Alfredo Deza <alfredodeza@gmail.com>
2013-06-27 15:12:01 -0400

    return better error codes back to lua

--- dreamobjects.lrplugin/s3.py
+++ dreamobjects.lrplugin/s3.py
@@ -1,16 +1,16 @@
 # The MIT License (MIT)
 # Copyright (c) 2013 Alfredo Deza
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -30,6 +30,21 @@ import boto
 import boto.s3.connection
 from boto.s3.key import Key
 
+
+def errors():
+    return {
+        400: 1,
+        403: 2,
+        405: 3,
+        409: 4,
+        411: 5,
+        412: 6,
+        416: 7,
+        501: 8,
+        503: 9,
+    }
+
+
 def parse_args(args):
     arguments = {'file_path': ''}
     args_map = {0: 'script', 1: 'command', 2: 'access_key', 3: 'secret_key', 4: 'bucket_name', 5: 'filename', 6: 'file_path'}
@@ -46,12 +61,17 @@ conn = boto.connect_s3(
         calling_format = boto.s3.connection.OrdinaryCallingFormat(),
         )
 
-if args['command'] == 'create':
-    bucket = conn.create_bucket(args['bucket_name'])
-    key = Key(bucket)
-    key.key = args['filename']
-    key.set_contents_from_filename(args['file_path'])
+try:
+    if args['command'] == 'create':
+        bucket = conn.create_bucket(args['bucket_name'])
+        key = Key(bucket)
+        key.key = args['filename']
+        key.set_contents_from_filename(args['file_path'])
 
-if args['command'] == 'delete':
-    bucket = conn.create_bucket(args['bucket_name'])
-    bucket.delete_key(args['filename'])
+    if args['command'] == 'delete':
+        bucket = conn.create_bucket(args['bucket_name'])
+        bucket.delete_key(args['filename'])
+except Exception as err:
+    if hasattr(err, 'status'):
+        raise SystemExit(errors().get(err.status))
+    raise
